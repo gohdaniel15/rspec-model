4ed684c24dc43ecfd075b7af93818eae3f85e59a
Daniel Goh <gohdaniel15@gmail.com>
2018-02-28 11:18:16 +0800

    RSpec and ShouldaMatchers setup

--- app/models/student.rb
+++ app/models/student.rb
@@ -5,7 +5,7 @@ class Student < ApplicationRecord
   validates :email, presence: true,
                     uniqueness: { case_sensitive: false }
   validates :github_url, presence: true,
-                         format: { with: /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/ix }
+                         format: { with: /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/ix, multiline: true }
   validates :twitter_url, presence: true
   validates :birth_date, presence: true
 
